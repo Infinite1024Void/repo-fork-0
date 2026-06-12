fc968d304426679b28282b0a21553d4e620b0710
InfiniteVoid <bplsm4jps@mozmail.com>
2026-06-12 09:22:41 -0400

    More QOL changes
    Some changes didn't get pushed so here's some more QOL changes.
    Changes are described in the PR.

---
+++ script.js
@@ -0,0 +1,27 @@
+const sidebar = document.querySelector("#sidebar");
+const toggleBtn = document.querySelector("#menuBtn");
+
+function toggleSidebar(e) {
+  e.stopPropagation(); // 👈 prevents body click
+  sidebar.classList.toggle("active");
+}
+
+const body = document.body;
+body.addEventListener("click", (e) => {
+  if (!sidebar.contains(e.target)) {
+    sidebar.classList.remove("active");
+  }
+});
+
+const mood = document.querySelector(".mood");
+const aboutMe = document.querySelector("#aboutMe");
+
+function changeMood() {
+  if (mood.innerHTML === "🌙") {
+    document.body.classList.add("dark");
+    mood.innerHTML = "🔆";
+  } else {
+    document.body.classList.remove("dark");
+    mood.innerHTML = "🌙";
+  }
+}
